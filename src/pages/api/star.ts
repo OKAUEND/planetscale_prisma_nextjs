db5e5d3111ccfbf1d99bd1977522ec28f994f8d9
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-09-14 22:59:41 +0900

    変更 - handler関数の型を作る

--- src/pages/api/star.ts
+++ src/pages/api/star.ts
@@ -1,1 +1,9 @@
+import { NextApiRequest } from "next";
 import prisma from "../../lib/prisma";
+
+type parameter = {
+  request: NextApiRequest;
+  response: NextApiRequest;
+};
+
+type handler = ({ request, response }: parameter) => void;
