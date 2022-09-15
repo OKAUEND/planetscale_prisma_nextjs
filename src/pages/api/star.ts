2a6ca6c78489084430f52da8eada69b51a3f9324
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-09-15 10:39:33 +0900

    変更 - prisma経由でPlanetScaleへ通信し値を取り出す部分の処理を記述

--- src/pages/api/star.ts
+++ src/pages/api/star.ts
@@ -1,9 +1,22 @@
-import { NextApiRequest } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../lib/prisma";
 
 type parameter = {
   request: NextApiRequest;
-  response: NextApiRequest;
+  response: NextApiResponse;
 };
 
-type handler = ({ request, response }: parameter) => void;
+type Handler = ({ request, response }: parameter) => void;
+
+const handler: Handler = async ({ request, response }) => {
+  const { method } = request;
+  switch (method) {
+    case "GET":
+      try {
+        const star = await prisma.star.findMany({});
+        response.status(200).json(star);
+      } catch (e) {}
+      break;
+    default:
+  }
+};
