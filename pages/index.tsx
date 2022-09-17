b278ebadcd25152485216fdd3fa83bbceb9f7c77
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-09-17 18:21:12 +0900

    変更 - fetch関数経由でAPIを叩くのを試しにやってみる

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    console.log("開始");
+    const fetchPlanetScale = async () => {
+      const response = await fetch("/api/star");
+      const data = await response.json();
+      console.log({ data });
+    };
+    fetchPlanetScale();
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
