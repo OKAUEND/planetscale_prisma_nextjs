11e934a09a72653e1e68014dff79902386e5bc9d
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-09-21 09:19:40 +0900

    変更 - お試しで、Vitestが動作するかを確認

--- src/feature/simple/index.ts
+++ src/feature/simple/index.ts
@@ -1,6 +1,12 @@
 export const getENV = () => {
-  return process.env.NODE_ENV;
+  return process.env.TEST_ENV;
 };
 
 if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+  describe("Vitest TEST", () => {
+    it("ENV GET TEST", () => {
+      expect(getENV()).toBe("test_env");
+    });
+  });
 }
