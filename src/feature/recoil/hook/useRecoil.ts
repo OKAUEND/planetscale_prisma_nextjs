35cccfdcd6e61b61d80fac949c9c95c66126ba5d
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-09-21 18:50:15 +0900

    変更 - Stateを作成

--- src/feature/recoil/hook/useRecoil.ts
+++ src/feature/recoil/hook/useRecoil.ts
@@ -2,4 +2,21 @@ import { atom, selector, atomFamily, selectorFamily } from "recoil";
 
 //---------------------------------------------
 
+const recoilAtom = atom({
+  key: "recoil-atom",
+  default: "",
+});
+
+//---------------------------------------------
+
+const recoilSelector = selector({
+  key: "recoil-atom",
+  get: ({ get }) => {
+    const recoil = get(recoilAtom);
+    return `${recoil} Selector`;
+  },
+});
+
+//---------------------------------------------
+
 export const useRecoil = () => {};
