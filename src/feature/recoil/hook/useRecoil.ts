f7d0d687e26758f38daa32498f801ac53768e242
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-09-23 14:50:37 +0900

    変更 - テストを追加

--- src/feature/recoil/hook/useRecoil.ts
+++ src/feature/recoil/hook/useRecoil.ts
@@ -1,3 +1,5 @@
+import { act, renderHook, waitFor } from "@testing-library/react";
+import { useCallback } from "react";
 import {
   atom,
   selector,
@@ -5,6 +7,9 @@ import {
   selectorFamily,
   useRecoilState,
   useRecoilValue,
+  RecoilRoot,
+  useRecoilCallback,
+  waitForAll,
 } from "recoil";
 
 //---------------------------------------------
@@ -29,6 +34,28 @@ const recoilSelector = selector({
 export const useRecoil = () => {
   const [recoil, setRecoil] = useRecoilState(recoilAtom);
   const recoilState = useRecoilValue(recoilSelector);
+  // const setRecoilAtom = useCallback((text) => setRecoil(text));
 
   return [recoil, recoilState, setRecoil] as const;
 };
+
+if (import.meta.vitest) {
+  describe("Recoil Custom hook TEST", () => {
+    it("Atom TEST", () => {
+      const { result } = renderHook(() => useRecoil(), { wrapper: RecoilRoot });
+      expect(result.current[0]).toEqual("");
+    });
+    it("Atom Update TEST", async () => {
+      const { result } = renderHook(() => useRecoil(), { wrapper: RecoilRoot });
+      const [recoil, , setRecoil] = result.current;
+
+      act(() => {
+        setRecoil("TEST");
+      });
+
+      await waitFor(() => expect(recoil).toEqual("TEST"));
+
+      // expect(recoil).toEqual("TEST");
+    });
+  });
+}
